636540146cfe40a8bff21296a22ba0a5953acaec
Charlie.Lee <c050820021a@gmail.com>
2026-03-26 15:29:48 +0800

    update term to uint32

--- internal/rpc/procedure.go
+++ internal/rpc/procedure.go
@@ -3,7 +3,7 @@ package rpc
 import "encoding/binary"
 
 type Frame struct {
-	RPCType    uint
+	RPCType    RPC_TYPE
 	RelationId uint16
 	Payload    []byte
 }
@@ -35,139 +35,143 @@ func DecodeRPCBase(bs []byte) *Frame {
 	return rb
 }
 
+type RpcPayload interface {
+	Encode() []byte
+}
+
 // byte layout:
 //
-//	Term -> 2 byte
+//	Term -> 4 byte
 //	LeaderId -> len
 //	LeaderCommit -> 4 byte
 //	PrevLogIndex -> 4 byte
-//	PrevLogTerm -> 2 byte
+//	PrevLogTerm -> 4 byte
 //	Entries -> len
 type AppendEntriesReq struct {
-	Term         uint16
+	Term         uint32
 	LeaderCommit uint32
 	PrevLogIndex uint32
-	PrevLogTerm  uint16
+	PrevLogTerm  uint32
 	LeaderId     string
 	Entries      []byte // up to the user to define how the entries are split, or not split, or each entry is just a byte. the rpc layer is only responsible for converting the incoming stream to a byte array
 }
 
 func (aer AppendEntriesReq) Encode() []byte {
 	leaderIdInBytes := []byte(aer.LeaderId)
-	out := make([]byte, 14+len(leaderIdInBytes)+len(aer.Entries))
+	out := make([]byte, 18+len(leaderIdInBytes)+len(aer.Entries))
 
-	binary.BigEndian.PutUint16(out[0:2], aer.Term)
-	binary.BigEndian.PutUint32(out[2:6], aer.LeaderCommit)
-	binary.BigEndian.PutUint32(out[6:10], aer.PrevLogIndex)
-	binary.BigEndian.PutUint16(out[10:12], aer.PrevLogTerm)
-	binary.BigEndian.PutUint16(out[12:14], uint16(len(leaderIdInBytes)))
-	copy(out[14:], leaderIdInBytes)
-	copy(out[14+len(leaderIdInBytes):], aer.Entries)
+	binary.BigEndian.PutUint32(out[0:4], aer.Term)
+	binary.BigEndian.PutUint32(out[4:8], aer.LeaderCommit)
+	binary.BigEndian.PutUint32(out[8:12], aer.PrevLogIndex)
+	binary.BigEndian.PutUint32(out[12:16], aer.PrevLogTerm)
+	binary.BigEndian.PutUint16(out[16:18], uint16(len(leaderIdInBytes)))
+	copy(out[18:], leaderIdInBytes)
+	copy(out[18+len(leaderIdInBytes):], aer.Entries)
 
 	return out
 }
 
 func DecodeAppendEntriesReq(bs []byte) *AppendEntriesReq {
 	aer := &AppendEntriesReq{
-		Term:         binary.BigEndian.Uint16(bs[0:2]),
-		LeaderCommit: binary.BigEndian.Uint32(bs[2:6]),
-		PrevLogIndex: binary.BigEndian.Uint32(bs[6:10]),
-		PrevLogTerm:  binary.BigEndian.Uint16(bs[10:12]),
+		Term:         binary.BigEndian.Uint32(bs[0:4]),
+		LeaderCommit: binary.BigEndian.Uint32(bs[4:8]),
+		PrevLogIndex: binary.BigEndian.Uint32(bs[8:12]),
+		PrevLogTerm:  binary.BigEndian.Uint32(bs[12:16]),
 	}
 
-	leaderIdLen := binary.BigEndian.Uint16(bs[12:14])
-	aer.LeaderId = string(bs[14 : 14+leaderIdLen])
-	aer.Entries = make([]byte, len(bs[14+leaderIdLen:]))
+	leaderIdLen := binary.BigEndian.Uint16(bs[16:18])
+	aer.LeaderId = string(bs[18 : 18+leaderIdLen])
+	aer.Entries = make([]byte, len(bs[18+leaderIdLen:]))
 
 	// The copy path is intentional:
 	// - Directly assigning the slice view keeps the underlying array alive, and risk unexpected alias problem
 	// - This does incur an extra copy, but as if any performance optimization guide will say, only optimise when it is actually the problem
-	copy(aer.Entries, bs[14+leaderIdLen:])
+	copy(aer.Entries, bs[18+leaderIdLen:])
 
 	return aer
 }
 
 // byte layout:
 //
-//	Term -> 2 byte
+//	Term -> 4 byte
 //	Success -> 1 byte
 type AppendEntriesRes struct {
-	Term    uint16
+	Term    uint32
 	Success bool
 }
 
 func (aer AppendEntriesRes) Encode() []byte {
-	out := make([]byte, 3)
+	out := make([]byte, 5)
 
-	binary.BigEndian.PutUint16(out[0:2], aer.Term)
-	out[2] = boolToByte(aer.Success)
+	binary.BigEndian.PutUint32(out[0:4], aer.Term)
+	out[4] = boolToByte(aer.Success)
 
 	return out
 }
 
 func DecodeAppendEntriesRes(bs []byte) *AppendEntriesRes {
 	return &AppendEntriesRes{
-		Term:    binary.BigEndian.Uint16(bs[0:2]),
-		Success: byteToBool(bs[2]),
+		Term:    binary.BigEndian.Uint32(bs[0:4]),
+		Success: byteToBool(bs[4]),
 	}
 }
 
 // byte layout:
 //
-//	Term -> 2 byte
+//	Term -> 4 byte
 //	LastLogIndex -> 4 byte
-//	LastLogTerm -> 2 byte
+//	LastLogTerm -> 4 byte
 //	CandidateId -> 1 byte
 type RequestVoteReq struct {
-	Term         uint16
+	Term         uint32
 	LastLogIndex uint32
-	LastLogTerm  uint16
+	LastLogTerm  uint32
 	CandidateId  string
 }
 
 func (rvr RequestVoteReq) Encode() []byte {
 	candidateIdInBytes := []byte(rvr.CandidateId)
-	out := make([]byte, 8+len(candidateIdInBytes))
+	out := make([]byte, 12+len(candidateIdInBytes))
 
-	binary.BigEndian.PutUint16(out[0:2], rvr.Term)
-	binary.BigEndian.PutUint32(out[2:6], rvr.LastLogIndex)
-	binary.BigEndian.PutUint16(out[6:8], rvr.LastLogTerm)
-	copy(out[8:], candidateIdInBytes)
+	binary.BigEndian.PutUint32(out[0:4], rvr.Term)
+	binary.BigEndian.PutUint32(out[4:8], rvr.LastLogIndex)
+	binary.BigEndian.PutUint32(out[8:12], rvr.LastLogTerm)
+	copy(out[12:], candidateIdInBytes)
 
 	return out
 }
 
 func DecodeRequestVoteReq(bs []byte) *RequestVoteReq {
 	return &RequestVoteReq{
-		Term:         binary.BigEndian.Uint16(bs[0:2]),
-		LastLogIndex: binary.BigEndian.Uint32(bs[2:6]),
-		LastLogTerm:  binary.BigEndian.Uint16(bs[6:8]),
-		CandidateId:  string(bs[8:]),
+		Term:         binary.BigEndian.Uint32(bs[0:4]),
+		LastLogIndex: binary.BigEndian.Uint32(bs[4:8]),
+		LastLogTerm:  binary.BigEndian.Uint32(bs[8:12]),
+		CandidateId:  string(bs[12:]),
 	}
 }
 
 // byte layout:
 //
-//	Term -> 2 byte
+//	Term -> 4 byte
 //	VoteGranted -> 1 byte
 type RequestVoteRes struct {
-	Term        uint16
+	Term        uint32
 	VoteGranted bool
 }
 
 func (rvr RequestVoteRes) Encode() []byte {
-	out := make([]byte, 3)
+	out := make([]byte, 5)
 
-	binary.BigEndian.PutUint16(out[0:2], rvr.Term)
-	out[2] = boolToByte(rvr.VoteGranted)
+	binary.BigEndian.PutUint32(out[0:4], rvr.Term)
+	out[4] = boolToByte(rvr.VoteGranted)
 
 	return out
 }
 
 func DecodeRequestVoteRes(bs []byte) *RequestVoteRes {
 	return &RequestVoteRes{
-		Term:        binary.BigEndian.Uint16(bs[0:2]),
-		VoteGranted: byteToBool(bs[2]),
+		Term:        binary.BigEndian.Uint32(bs[0:4]),
+		VoteGranted: byteToBool(bs[4]),
 	}
 }
 
